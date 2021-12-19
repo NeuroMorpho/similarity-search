cc2ccb45abc39e0dd157bc37036214870a5a7945
Bengt Ljungquist <bljungqu@gmu.edu>
2021-12-19 14:13:20 -0500

    Clean config file added

---
+++ sis/cfg.py
@@ -0,0 +1,35 @@
+basemetadir = ''
+basereleasedir = ''
+pathonserver = ''
+
+# Define nonsense data and mismatch dataframe
+nondata = {'.DS_Store','._.DS_Store','desktop.ini'}
+html_head = '''
+<html>
+  <head><title>Test report</title>
+  <script src="js/sorttable.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/df_style.css"/>
+
+  
+  </head>
+  <body>
+'''
+html_footer = '''
+  </body>
+</html>.
+'''
+
+keyfile_path= ""
+
+host = ''
+port = 22
+username = ''
+
+
+dbhost = ''
+dbuser = ''
+dbpass = ''
+dbsel = ''
+dbtimeout = 500000
+
+
